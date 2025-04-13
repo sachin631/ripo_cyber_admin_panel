f38e92696b10851653bf8b2f1ba4ad803b66c0a4
Sangwan <84963232+sachin631@users.noreply.github.com>
2025-04-13 20:26:08 +0530

    Update apiClient.ts

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api/v1',
+  baseURL: 'http://3.6.206.136:3001/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
